9b54717bb6450fe4883641194da51b1669ec4433
ricodroid404 <ricodroid2024@gmail.com>
2024-03-08 16:46:40 +0900

    first commit

---
+++ story_board_template/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  story_board_template
+//
+//  Created by r_murata on 2024/03/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
